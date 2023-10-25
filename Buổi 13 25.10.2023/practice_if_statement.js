6ddb085e8d638b3c7d8cc1da1822dfc817e0ca95
dinhduynguyen2008 <nguyendinhduy.0891@gmail.com>
2023-10-25 22:32:28 +0700

    Bài tập trên Learn

---
+++ Buổi 13 25.10.2023/practice_if_statement.js
@@ -0,0 +1,17 @@
+let btnActionElem = document.getElementById("kiểm-tra");
+
+btnActionElem.addEventListener("click",() => {
+    let gioLamElem = parseFloat(document.getElementById("giolam").value);
+    let capNvElem = parseFloat(document.getElementById("capNv").value);
+    let ketQuaElem = document.getElementById("info");
+  
+    if (gioLamElem > 400 && capNvElem == 1) {
+        ketQuaElem = ("Bạn được thưởng 1000USD");
+    } else if (gioLamElem > 400 && capNvElem == 2) {
+        ketQuaElem = ("Bạn được thưởng 2000USD");
+    } else {
+        ketQuaElem = ("Bạn chưa đủ điều kiện thưởng");
+    }
+
+    document.getElementById("info").innerHTML = ketQuaElem;
+})
