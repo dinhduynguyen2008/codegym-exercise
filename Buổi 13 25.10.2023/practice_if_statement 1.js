2a8156043869ff299009081840d1e400fbfd3084
dinhduynguyen2008 <nguyendinhduy.0891@gmail.com>
2023-10-30 21:49:25 +0700

    them file Buổi 14 30.10.2023

--- Buổi 13 25.10.2023/practice_if_statement 1.js
+++ Buổi 13 25.10.2023/practice_if_statement 1.js
@@ -4,14 +4,16 @@ btnActionElem.addEventListener("click",() => {
     let gioLamElem = parseFloat(document.getElementById("giolam").value);
     let capNvElem = parseFloat(document.getElementById("capNv").value);
     let ketQuaElem = document.getElementById("info");
-  
-    if (gioLamElem > 400 && capNvElem == 1) {
+  if (gioLamElem > 400) {
+
+  if (capNvElem == 1) {
         ketQuaElem = ("Bạn được thưởng 1000USD");
-    } else if (gioLamElem > 400 && capNvElem == 2) {
-        ketQuaElem = ("Bạn được thưởng 2000USD");
     } else {
+        ketQuaElem = ("Bạn được thưởng 2000USD");
+  }
+    }else {
         ketQuaElem = ("Bạn chưa đủ điều kiện thưởng");
-    }
+  }
 
     document.getElementById("info").innerHTML = ketQuaElem;
 });
