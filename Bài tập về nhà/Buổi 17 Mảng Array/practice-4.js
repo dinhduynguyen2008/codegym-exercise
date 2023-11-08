341b28b4f36c701bd871f6bd3931b2ac468cbf68
dinhduynguyen2008 <nguyendinhduy.0891@gmail.com>
2023-11-08 21:33:06 +0700

    thêm file bài tập

--- Bài tập về nhà/Buổi 17 Mảng Array/practice-4.js
+++ Bài tập về nhà/Buổi 17 Mảng Array/practice-4.js
@@ -1,15 +1,11 @@
 // Bài 4: Cho 2 array có cùng số phần tử với nhau. 
 let array1 = [10, 20, 30, 40];
 let array2 = [100, 200, 300, 400];
-
+array2.reverse()
 let array3 = [ ]
-if (array1.length === array2.length) {
-    for (let i = 0; i < array1.length; i++) {
-       
-     array3.push(array1[i] + array2[array2 - 1 - i])
-    }
-document.write(array3);  
-}
+for (let i in array1){
+    document.write(`${array1[i]} ${array2[i]} <br>`)
+   }
 // Hãy in các thành phần trong 2 array theo format dưới đây (sử dụng document.write)
 /**
  * 10 400
--- Bài tập về nhà/Buổi 17 Mảng Array/practice-7.js
+++ Bài tập về nhà/Buổi 17 Mảng Array/practice-7.js
@@ -2,5 +2,9 @@
 let array1 = ["Hello ", "take "];
 let array2 = ["Dear", "Sir"];
 let array3 = [];
-
+for (let i in array1) {
+    for (let j in array2) {
+        array3.push(array1[i] + array2 [j])
+    }
+}
 console.log(array3); // Output: ["Hello Dear", "Hello Sir", "take Dear", "take Sir"]
